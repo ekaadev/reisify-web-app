987f07bdeffb247992594ffee660da3908f96369
ekaadev <ekaadev9@gmail.com>
2026-04-03 08:31:54 +0700

    fix: standardize import quotes and formatting across multiple files

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/consistent-type-specifier-style */
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
